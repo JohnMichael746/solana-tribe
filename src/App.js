8ce016b4634bab99ab0f06470ef426692990a7b3
johnmichael <withyou746@gmail.com>
2024-02-29 14:16:27 -0500

    layout changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 function App() {
+  const tokenAddress = "7njsg9BA1xvXX9DNpe5fERHK4zb7MbCHKZ6zsx5k3adr";
+
   return (
     <>
       <img className="object-cover min-h-[100vh] w-full absolute -z-[1] max-sm:opacity-70" src="assets/images/sun.gif" alt="background" />
@@ -6,8 +8,8 @@ function App() {
         <header className="flex items-center justify-between">
           <div className="flex items-center gap-8 max-sm:gap-4 h-24 max-sm:h-10 my-5 text-[#531D01]">
             <img className="h-full rounded-full" src="logo.png" alt="tribe" />
-            <a target="_blank" className="text-[40px] max-sm:text-lg cursor-pointer animation hover:text-hover">Home</a>
-            <a href="/" target="_blank" className="text-[40px] max-sm:text-lg cursor-pointer animation hover:text-hover">Chart</a>
+            <a target="_blank" className="font-digit text-[40px] max-sm:text-lg cursor-pointer animation hover:text-hover">Home</a>
+            <a href={`https://dexscreener.com/solana/${tokenAddress}`} target="_blank" className="font-digit text-[40px] max-sm:text-lg cursor-pointer animation hover:text-hover">Chart</a>
           </div>
           <a href="/" target="_blank">
             <img className="h-[70px] max-sm:h-[40px] object-contain cursor-pointer hover:scale-95 transition-all" src="assets/images/btnTelegram.png" alt="button" />
@@ -17,29 +19,42 @@ function App() {
         <div className="flex items-center justify-between max-sm:flex-col-reverse max-sm:text-center">
           <div>
             <img className="w-[500px] max-sm:w-[75%] -ml-[30px] max-sm:m-auto" src="assets/images/title.gif" alt="title" />
-            <p className="text-[40px] max-sm:text-[25px] leading-tight text-stroke text-[#FFC700]">Fueling the journey to 1 billion market cap</p>
-            <a href="/" target="_blank">
+            <p className="font-digit text-[40px] max-sm:text-[25px] leading-tight text-stroke text-[#FFC700]">
+              Join the Tribe on it's Mission to 1 Billion Market Cap
+            </p>
+            <a className="w-fit" href={`https://raydium.io/swap/?inputCurrency=sol&outputCurrency=${tokenAddress}&fixed=in`} target="_blank">
               <img className="w-[350px] -ml-[20px] max-sm:m-auto max-sm:w-[70%]" src="assets/images/btnBuy.gif" alt="title" />
             </a>
+            <div className="text-[20px] text-stroke text-white max-sm:w-[320px] max-sm:m-auto max-w-[500px] break-words max-sm:text-[14px] max-sm:mb-[40px]">
+              Contract : {tokenAddress}
+            </div>
           </div>
-          <img className="w-[60%] -mr-[100px] max-sm:m-auto max-sm:w-[90%] cursor-pointer" src="assets/images/banner.png" alt="banner" />
+          <img className="max-sm:hidden w-[55%] -mr-[100px] max-sm:m-auto max-sm:w-[90%] cursor-pointer" src="assets/images/banner.png" alt="banner" />
         </div>
       </div>
 
-      <div className="py-10 bg-[#00000080] flex-col items-center justify-center">
-        <div className="flex items-center justify-center mb-10 max-sm:flex-col max-sm:gap-5">
-          <div className="text-[40px] text-[#FFC700] text-stroke mr-20 max-sm:text-[24px] max-sm:m-auto">Join The Tribe</div>
-          <a href="/" target="_blank"><img className="w-[250px] max-sm:w-[150px] cursor-pointer" src="assets/images/btnJoin.png" alt="join" /></a>
+      <div className="py-10 bg-[#00000080] flex-col items-center justify-center mt-[120px] max-sm:mt-[20px] max-sm:py-8">
+        <div className="flex items-center justify-center max-sm:flex-col max-sm:gap-2">
+          <div className="text-[40px] text-[#FFC700] text-stroke mr-20 max-sm:text-[16px] max-sm:m-auto">
+            Join The Tribe
+          </div>
+          <a href="/" target="_blank">
+            <img className="w-[250px] max-sm:w-[150px] cursor-pointer" src="assets/images/btnJoin.png" alt="join" />
+          </a>
         </div>
-        <p className="text-stroke text-[#D0C28D] text-[28px] max-w-[900px] m-auto text-center px-5 max-sm:text-[18px]">Empowering communities with a digital currency that fosters unity and prosperity.</p>
       </div>
 
-      <div className="text-[30px] text-white m-auto text-center break-words my-[100px] text-stroke px-5 max-sm:text-[18px] max-sm:my-[40px]">
-        Contract : 7njsg9BA1xvXX9DNpe5fERHK4zb7MbCHKZ6zsx5k3adr
+      <img className="mt-[100px] max-sm:mt-[60px] h-full rounded-full m-auto max-sm:h-[60px]" src="logo.png" alt="tribe" />
+
+      <div className="w-full text-center px-5 text-[40px] max-sm:text-[20px] flex items-center justify-center gap-5 mt-8">
+        <img src="assets/images/instagram.svg" alt="instagram" />
+        <img src="assets/images/twitter.svg" alt="twitter" />
+        <img src="assets/images/telegram.svg" alt="telegram" />
       </div>
 
-      <div className="w-full text-center px-5 text-[40px] max-sm:text-[20px]">💰💰💰💰</div>
-      <div className="w-full text-center px-5 text-[30px] text-[#DC8544] text-stroke my-[30px] max-sm:text-[14px]">© 2024 Tribe Token. All rights reserved.</div>
+      <div className="w-full text-center px-5 text-[16px] max-sm:text-[12px] text-white my-[30px]">
+        © 2024 Tribe Token. All rights reserved.
+      </div>
     </>
   );
 }
